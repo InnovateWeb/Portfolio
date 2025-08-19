98d43566240b92125a39e56d5f911a3aa4fcc0d2
Maxime Séchaud <maxime.sechaud@sectionmediamatique.ch>
2025-08-19 10:49:07 +0200

    Version 1.5

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,11 @@
 import styles from "../styles/footer.module.css";
 
 export default function Footer() {
+    const currentYear = new Date().getFullYear();
 
     return (
         <footer className={styles.footer}>
-            <p>Une réalisation signée Maxime Séchaud © 2025</p>
+            <p>Une réalisation signée Maxime Séchaud © {currentYear}</p>
         </footer>
     );
 }
